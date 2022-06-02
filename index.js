76bccd1c52d7f3884d5da6c274e7be1b66d87fd0
Aniket Wakchaure <aniket.wakchaure@morningstar.com>
2022-06-02 16:54:18 +0530

    FirstemployeeApp

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello world")
+//Importing express libraries to create server
+const express = require("express")
+
+const app=express()
+
+app.get("/user",(req,res)=>{
+    res.send("First API Call")
+})
+const port=5000;
+
+app.listen(port,(req,res)=>{
+    console.log(`Starting Application on port no: ${port}`)
+})
